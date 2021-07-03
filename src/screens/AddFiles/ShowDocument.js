e24b13354fa44b0f8135e409b2b04122c067f274
Naim Lasker <naim-lasker>
2021-07-04 03:05:45 +0600

    file get & downloaded

--- src/screens/AddFiles/ShowDocument.js
+++ src/screens/AddFiles/ShowDocument.js
@@ -1,38 +1,87 @@
-import React, { useState } from "react"
-import { ScrollView, TextInput, TouchableOpacity } from "react-native"
-import Toast from 'react-native-simple-toast'
+import React, { useEffect, useState } from "react"
+import { TouchableOpacity, FlatList } from "react-native"
 import Block from '../../components/Block'
 import Text from '../../components/Text'
 import styles from './styles'
-import { Navbar } from "../../layouts/Navbar"
-import AdsCarousel from '../Carousel'
-import { SecondaryButton } from '../../components/Button'
+import { fetchFilesService, downloadFileService } from '../../services/file'
 import { colors } from "../../styles/theme"
+import { useDispatch } from "react-redux"
+import { Loader } from "../../components/Loader"
+import RNFetchBlob from 'rn-fetch-blob'
+import { Config } from "../../config"
 
 
 export default ShowDocument = ({ navigation }) => {
+    const dispatch = useDispatch()
+    const [loading, setLoading] = useState(true)
+    const [files, setFiles] = useState([])
 
+    console.log('files', files);
+
+    useEffect(() => {
+        getFiles()
+    }, [])
+
+    const getFiles = () => {
+        dispatch(fetchFilesService((res, err) => {
+            setLoading(false)
+            setFiles(res?.data?.data ? JSON.parse(res.data.data) : [])
+        }))
+    }
+
+    const renderFiles = ({ item, index }) => (
+        <Block style={styles.table}>
+            <Text style={styles.td}>{item.title}</Text>
+            <Text style={styles.td}>{item.desc}</Text>
+            <TouchableOpacity style={styles.td}>
+                <Text color={colors.primaryColor}>View</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.td} onPress={() => downloadFile(item.filename)}>
+                <Text color={colors.primaryColor}>Download</Text>
+            </TouchableOpacity>
+        </Block>
+    )
+
+
+    const downloadFile = (filename) => {
+        const { config, fs } = RNFetchBlob
+        let PictureDir = fs.dirs.PictureDir
+        let options = {
+            fileCache: true,
+            addAndroidDownloads: {
+                useDownloadManager: true,
+                notification: false,
+                path: PictureDir + "/ag_" + new Date(),
+                description: 'Downloading image.'
+            }
+        }
+
+        config(options).fetch('GET', Config.base_url + `/file/${filename}`).then((res) => {
+            console.log('File downloaded')
+        })
+    }
 
 
     return (
 
-        <Block block padding={[30,0]}>
+        <Block block padding={[30, 0]}>
             <Block style={styles.table}>
                 <Text style={styles.td}>Title</Text>
                 <Text style={styles.td}>Description</Text>
                 <Text style={styles.td}>View File</Text>
                 <Text style={styles.td}>Download File</Text>
             </Block>
-            <Block style={styles.table}>
-                <Text style={styles.td}>aaa</Text>
-                <Text style={styles.td}>sss</Text>
-                <TouchableOpacity style={styles.td}>
-                    <Text color={colors.primaryColor}>View</Text>
-                </TouchableOpacity>
-                <TouchableOpacity style={styles.td}>
-                    <Text color={colors.primaryColor}>Download</Text>
-                </TouchableOpacity>
-            </Block>
+
+            {
+                loading ? <Loader /> :
+                    <FlatList
+                        showsVerticalScrollIndicator={false}
+                        data={files}
+                        renderItem={renderFiles}
+                        keyExtractor={item => item._id.$oid.toString()}
+                    />
+            }
+
         </Block>
 
     );
---
+++ src/services/file.js
@@ -0,0 +1,101 @@
+import { Config } from '../config/index'
+import httpRequest from '../utils/request'
+import auth from '../utils/auth'
+import {
+    GET_FILES_PENDING, GET_FILES_SUCCESS, GET_FILES_FAIL,
+    ADD_FILE_PENDING, ADD_FILE_SUCCESS, ADD_FILE_FAIL,
+} from '../constant/fileConstant'
+
+const base_url = Config.base_url
+
+
+
+/**
+ * Method: GET
+ */
+
+
+export const fetchFilesService = (callback) => async (
+    dispatch,
+    getState
+) => {
+    dispatch({ type: GET_FILES_PENDING })
+
+    const url = base_url + '/getAllFiles/60b1fbf90f2d2ea3dd2dc72a'
+    const token = await auth.getToken('accessToken')
+
+    try {
+        const response = await httpRequest.get(
+            url,
+            true,
+            token,
+            false,
+            null
+        )
+
+        dispatch({
+            type: GET_FILES_SUCCESS,
+            payload: response,
+        })
+
+        callback(response, null)
+    } catch (error) {
+        dispatch({
+            type: GET_FILES_FAIL,
+            payload: error.response,
+        })
+
+        callback(null, error.response)
+    }
+}
+
+/**
+ * Method: POST
+ */
+
+
+export const addFileService = (formData, callback) => async (
+    dispatch,
+    getState
+) => {
+    dispatch({ type: ADD_FILE_PENDING })
+
+    const url = base_url + 'file_upload'
+    const token = await auth.getToken('accessToken')
+
+    const data = {
+        file: formData.file,
+        title: formData.title,
+        description: formData.description,
+        filedata: formData.filedata,
+    }
+
+    const option = {
+        'Content-Type': 'application/json',
+    };
+
+
+    try {
+        const response = await httpRequest.post(
+            url,
+            true,
+            token,
+            JSON.stringify(data),
+            option
+        )
+
+        dispatch({
+            type: ADD_FILE_SUCCESS,
+            payload: response,
+        })
+
+        callback(response, null)
+    } catch (error) {
+        dispatch({
+            type: ADD_FILE_FAIL,
+            payload: error.response,
+        })
+
+        callback(null, error.response)
+    }
+}
